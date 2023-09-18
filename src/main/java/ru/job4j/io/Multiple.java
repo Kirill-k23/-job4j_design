9efa48df9c0619400f11db9f3d8527aec51f1fa8
Kiril-k23 <kirillkleyn@gmail.com>
2023-09-18 18:43:07 +0300

    2.0. FileOutputStream [#252488 #445962]

--- src/main/java/ru/job4j/io/Multiple.java
+++ src/main/java/ru/job4j/io/Multiple.java
@@ -1,6 +1,7 @@
 package ru.job4j.io;
 
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 public class Multiple {
@@ -24,7 +25,7 @@ public class Multiple {
             out.write(System.lineSeparator().getBytes());
             out.write("1*10=10".getBytes());
             out.write(System.lineSeparator().getBytes());
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
