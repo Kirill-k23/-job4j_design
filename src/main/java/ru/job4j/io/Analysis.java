c457243c73cc4fcac400a17457a2b35251c6d544
Kiril-k23 <kirillkleyn@gmail.com>
2023-09-23 00:15:35 +0300

    7. Анализ доступности сервера [#859]

---
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -0,0 +1,30 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class Analysis {
+    public void unavailable(String source, String target) {
+        try (BufferedReader reader = new BufferedReader(
+                new FileReader(source));
+             BufferedWriter writer = new BufferedWriter(
+                     new FileWriter(target));
+        ) {
+            String line;
+            boolean con = false;
+            while ((line = reader.readLine()) != null) {
+                String[] strings = line.split(" ");
+                if (con != Integer.parseInt(strings[0]) >= 500) {
+                    con = true;
+                    writer.append(strings[1]).append(";");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Analysis analysis = new Analysis();
+        analysis.unavailable("data/server.log", "data/target.csv");
+    }
+}
