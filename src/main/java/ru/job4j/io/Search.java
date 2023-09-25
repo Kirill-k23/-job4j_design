1533ed0cf60406afde952c4530e94e2a78857c4d
Kiril-k23 <kirillkleyn@gmail.com>
2023-09-26 00:13:22 +0300

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -10,7 +10,8 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
+        validate(args);
+        Path start = Paths.get(args[0]);
         search(start, p -> p.toFile().getName().startsWith("Simple")).forEach(System.out::println);
     }
 
@@ -18,6 +19,19 @@ public class Search {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getList();
+    }
+
+    public static void validate(String[] args) {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Root folder isn't correct. Usage  ROOT_FOLDER.");
+        }
+        Path start = Paths.get(args[0]);
+        if (!Files.exists(start)) {
+            throw new IllegalArgumentException(String.format("%s doesn't exist", start.toAbsolutePath()));
+        }
+        if (!Files.isDirectory(start)) {
+            throw new IllegalArgumentException(String.format("%s isn't a directory", start.toAbsolutePath()));
+        }
     }
 }
 
