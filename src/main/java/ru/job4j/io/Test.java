1ee426f3f35f66d4656fd7424962ab59509b4076
Kiril-k23 <kirillkleyn@gmail.com>
2023-10-04 16:54:02 +0300

    3. Формат XML [#313165]

---
+++ src/main/java/ru/job4j/io/Test.java
@@ -0,0 +1,10 @@
+package ru.job4j.io;
+
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello".substring(0, 2)
+        );
+    }
+}
