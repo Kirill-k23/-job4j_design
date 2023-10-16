b4240827870ef3540fe02ab4a493c14375d297c8
Kiril-k23 <kirillkleyn@gmail.com>
2023-10-16 19:58:41 +0300

    1. Связи между таблицами [#373615]

--- src/db/one_to_one.sql
+++ src/db/one_to_one.sql
@@ -3,8 +3,8 @@ id serial primary key,
 name varchar(255)
 );
 
-create table ticket (
+create table ticket(
 id serial primary key,
 number int,
-passenger_id int references passenger(id)
+passenger_id unique references passenger(id)
 );
